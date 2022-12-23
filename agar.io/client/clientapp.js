67bb4cb99fce21a02f2bafcc9c17600c7918ff46
RafaelOudshoorn <rafaeloud@gmail.com>
2022-12-23 02:41:53 +0100

    .

--- agar.io/client/clientapp.js
+++ agar.io/client/clientapp.js
@@ -13,30 +13,40 @@ var getUrlParameter = function getUrlParameter(sParam) {
     return false;
 };
 var dt = new Date();
-var debugtime = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 
 let ws = null;
 function connectWS() {
     ws = new WebSocket('ws://localhost:8082/ws');
     
     ws.onopen = function(){
-        console.log('%c['+debugtime+']  Connected to server' ,'background: #222; color: #bada55');
+        console.log('%c['+time+']  Connected to server' ,'background: #222; color: #bada55');
         ws.send(JSON.stringify({
             "action": "setusername",
             "data": $("#nickname").val(),
             "room": $("#room").val()
         }));
-        $("#join-game").css("display","none");
+        $("#join-game").remove();
+        $("#connected").css("display", "block");
     }
     ws.addEventListener('message', (event) => {
-        console.log(event.data);
+        // console.log(event.data);
     });
     ws.onmessage = function(websocketData) {
         const data = JSON.parse(websocketData.data);
-        if(data.action == "updateChat"){
-            $("#chat").append("<span style='color:white'>["+data.username+"]:</span> "+data.data+"<br>");
-            console.log(data.data);
+        switch(data.action){
+            case "updateChat":
+                $("#chat").append("<span style='color:blue'>["+data.username+"]:</span> "+data.data+"<br>");
+            break;
+            case "playerJoinShowChat":
+                $("#chat").append("<span style='color:yellow'>"+data.data+" joined</span><br>");
+            break;
+            case "playerDisconnectShowChat":
+                $("#chat").append("<span style='color:yellow'>"+data.data+" disconnected</span><br>");
+                
+            break;
         }
+        // console.log('['+time+'] '+data.data);
     }
 }
 
--- agar.io/client/index.php
+++ agar.io/client/index.php
@@ -7,52 +7,24 @@
         <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <script rel="javascript" src="clientapp.js"></script>
+        <link rel="stylesheet" href="style.css">
         <title>NodeJS</title>
         <script>
             $(document).ready(function(){
-                
+                for(var i = 1222;i > 0; i --){
+                    $("#grid-container").append("<div id='grid-item'></div>");
+                }
+                for(var u = 1000;u > 0; u --){
+                    $('#game').append('<div id="particle" class="particle'+u+'"></div>');
+                    var randomTop = Math.random() * 875;
+                    var randomLeft = Math.random() * 1590;
+                    $('.particle'+u).css({
+                        top: randomTop,
+                        left: randomLeft
+                    });
+                }
             });
         </script>
-        <style>
-            body{
-                height: 100vh;
-                width: 100%;
-                background: #868686;
-                overflow: hidden;
-                padding-top: 50px;
-            }
-            #join-game div{
-                width: 400px;
-            }
-            #list-lobbys{
-                background: darkgray;
-                height: auto;
-                width: 400px;
-            }
-            #chat{
-                color: black;
-                position: absolute;
-                left: 8px;
-                bottom: 40px;
-                min-width: 300px;
-                max-width: 500px;
-                max-height: 500px;
-                overflow: hidden;
-            }
-            #chat p{
-                /* background-color: #FFF; */
-                min-width: 300px;
-                max-width: 500px;
-            }
-            #message-form{
-                position: absolute;
-                left: 5px;
-                bottom: 5px;
-            }
-            #message-form input{
-                width: 448px;
-            }
-        </style>
     </head>
     <body>
         <center id="join-game">
@@ -60,18 +32,25 @@
                 <h1>Agar.io &#60;clone&#62;</h1>
                 <h3>Join Game</h3>
                 <label for="nickname">Nickname</label>
-                <input type="text" class="form-control" id="nickname" value="piet">
+                <input type="text" class="form-control" id="nickname" value="nickname">
                 <label for="room">Room code</label>
                 <input type="text" class="form-control" id="room" value="1234">
+                <br>
                 <button class="btn btn-primary" id="connect-btn">Verbinden</button>
             </div>
             <br>
             <div id="list-lobbys"><h3>Room Codes</h3></div>
         </center>
-        <div id="message-form"> 
-            <input type="text" id="message-input" placeholder="Enter message here">
+        <div id="connected">
+            <div id="game-container">
+                <div id="grid-container"></div>
+                <div id="game"></div>
+            </div>
+            <div id="points">Points: 00000</div>
+            <div id="scorebord"><h6>Scorebord</h6></div>
+            <div id='chat'></div>
+            <input type='text' id='message-input' placeholder='Enter message here'>
         </div>
-        <div id="chat"></div>
         <script>
             $("#connect-btn").click(function(){
                 connectWS();
@@ -92,13 +71,6 @@
                         }));
                         $("#message-input").val("");
                     }
-                }
-            });
-            const display = $('#join-game').css('display');
-            $("#goto-create-game").click(function(){
-                if (display === 'block') {
-                    $('#join-game').css('display','none');
-                    $('#create-game').css('display','block');
                 }
             });
         </script>
